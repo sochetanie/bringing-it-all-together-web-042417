b973ec063321ef3a78af01f8ea6fe7d99c8ca904
sochetanie <allagorik2901@gmail.com>
2017-05-02 19:03:40 -0400

    d

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment'
 DB[:conn] = SQLite3::Database.new ":memory:"
 
+
 RSpec.configure do |config|
   # Use color not only in STDOUT but also in pagers and files
   config.tty = true
